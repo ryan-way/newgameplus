61ab403b4c0ab0310af6be08e59e761284610bf7
ryan-way <ryan.keith.way@gmail.com>
2022-09-08 21:37:59 -0700

    updated test for sqlite to stop inserting

--- src/electron/main.ts
+++ src/electron/main.ts
@@ -23,17 +23,11 @@ let mainWindow;
 function initializeTypeOrm() {
   AppDataSource.initialize().then(async () => {
 
-      console.log("Inserting a new user into the database...")
-      const user = new User()
-      user.firstName = "Timber"
-      user.lastName = "Saw"
-      user.age = 25
-      await AppDataSource.manager.save(user)
-      console.log("Saved a new user with id: " + user.id)
-
       console.log("Loading users from the database...")
-      const users = await AppDataSource.manager.find(User)
-      console.log("Loaded users: ", users)
+      const user = await AppDataSource.manager.findOneBy(User, {
+        id: 1,
+      });
+      console.log("Loaded users: ", user ?? 'none');
 
       console.log("Here you can setup and run express / fastify / any other framework.")
 

6b88a155d1e67dc1cfaf39ef16e189bc1c5cba2a
ryan-way <ryan.keith.way@gmail.com>
2022-11-06 22:23:03 -0800

    closes #86; closes #74: refactoring main process

--- src/electron/main.ts
+++ src/electron/main.ts
@@ -1,80 +1,11 @@
 // Require for typeorm
 import 'reflect-metadata';
-import windowStateManager from 'electron-window-state';
 import contextMenu from 'electron-context-menu';
-import { app, BrowserWindow, ipcMain } from 'electron';
-import serve from 'electron-serve';
-import path from 'path';
-
-import db from './data-source';
-import { User } from './data-source';
+import { ipcMain } from 'electron';
+import windowService from './window-service';
 
 import { router } from './trpc';
-import { PrismaClient } from '@prisma/client';
 import { createIPCHandler } from 'electron-trpc';
-
-const serveURL = serve({ directory: '.' });
-const port = process.env.PORT || 3000;
-const dev = !app.isPackaged;
-let mainWindow;
-
-class Main {
-  async start() {
-    const prisma = new PrismaClient();
-
-    const count = await prisma.count.findFirst();
-    console.log(count);
-    createIPCHandler({ ipcMain, router });
-  }
-}
-
-const main = new Main();
-main.start();
-
-function createWindow() {
-  const windowState = windowStateManager({
-    defaultWidth: 800,
-    defaultHeight: 600,
-  });
-
-  const mainWindow = new BrowserWindow({
-    backgroundColor: 'whitesmoke',
-    titleBarStyle: 'hidden',
-    autoHideMenuBar: true,
-    trafficLightPosition: {
-      x: 17,
-      y: 32,
-    },
-    minHeight: 450,
-    minWidth: 500,
-    webPreferences: {
-      contextIsolation: true,
-      nodeIntegration: true,
-      spellcheck: false,
-      devTools: true,
-      preload: path.join(__dirname, '..', 'preload', 'preload.cjs'),
-    },
-    x: windowState.x,
-    y: windowState.y,
-    width: windowState.width,
-    height: windowState.height,
-  });
-
-  mainWindow.webContents.openDevTools();
-
-  windowState.manage(mainWindow);
-
-  mainWindow.once('ready-to-show', () => {
-    mainWindow.show();
-    mainWindow.focus();
-  });
-
-  mainWindow.on('close', () => {
-    windowState.saveState(mainWindow);
-  });
-
-  return mainWindow;
-}
 
 contextMenu({
   showLookUpSelection: false,
@@ -87,35 +18,13 @@ contextMenu({
   ],
 });
 
-function loadVite(port) {
-  mainWindow.loadURL(`http://localhost:${port}`).catch(e => {
-    console.log('Error loading URL, retrying', e);
-    setTimeout(() => {
-      loadVite(port);
-    }, 200);
-  });
-}
+class Main {
+  async start() {
+    createIPCHandler({ ipcMain, router });
 
-function createMainWindow() {
-  mainWindow = createWindow();
-  mainWindow.once('close', () => {
-    mainWindow = null;
-  });
-
-  if (dev) loadVite(port);
-  else serveURL(mainWindow);
-}
-
-app.once('ready', createMainWindow);
-app.on('activate', () => {
-  if (!mainWindow) {
-    createMainWindow();
+    windowService.startMainWindow();
   }
-});
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') app.quit();
-});
+}
 
-ipcMain.on('to-main', (event, count) => {
-  return mainWindow.webContents.send('from-main', `next count is ${count + 1}`);
-});
+const main = new Main();
+main.start();

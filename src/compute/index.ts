a6958bf43ef2375ae3ebaebed14b13754674844e
ryan-way <ryan.keith.way@gmail.com>
2022-11-08 21:56:40 -0800

    compute process fetches count data from main

--- src/compute/index.ts
+++ src/compute/index.ts
@@ -1,3 +1,10 @@
 import trpcClient from '../trpc/client';
 
-trpcClient.mutation('compute-count', 9);
+async function main() {
+  console.log('Compute Main');
+  console.log('fetching num...');
+  const count: number = await trpcClient.query('compute-get-count');
+  console.log('returning num...');
+  trpcClient.mutation('compute-count', count + 1);
+}
+main();
